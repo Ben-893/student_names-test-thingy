911d3be53edf24e671adafc42fed6c3adf1af3ab
Benjamin <ben.smith.gordon@gmail.com>
2018-06-12 13:54:31 +0100

    Student names

---
+++ lib/student_name.rb
@@ -0,0 +1,9 @@
+user_input = gets.chomp
+names = []
+
+def student_name(user_input)
+  user_input = gets.chomp
+  names << user_input
+end
+
+puts names
---
+++ spec/student_name_spec.rb
@@ -0,0 +1,7 @@
+require "./lib/student_name"
+describe "student_name" do
+
+  it "takes a name and stores it in an array" do
+    expect(student_name(user_input)).to eq ""
+  end
+end
